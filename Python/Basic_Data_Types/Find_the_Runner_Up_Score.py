8273e9b6139be1dc0b8c35d0d9c918ccee366900
pawelk82 <pawelk82@gmail.com>
2018-06-19 15:03:29 +0300

    add

---
+++ Python/Basic_Data_Types/Find_the_Runner_Up_Score.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+#Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. 
+#Store them in a list and find the score of the runner-up.
+
+#Input Format
+
+#The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+
+#Output Format
+
+#Print the runner-up score.
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = set(map(int, input().split()))
+    arr.remove(max(arr))
+    print (max(arr))  
