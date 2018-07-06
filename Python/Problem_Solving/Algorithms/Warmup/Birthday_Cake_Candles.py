6cb7d822d700f425c04de5d64b32daa637a679a4
pawelk82 <pawelk82@gmail.com>
2018-07-06 19:41:03 +0300

    add

--- Python/Problem_Solving/Algorithms/Warmup/Birthday_Cake_Candles.py
+++ Python/Problem_Solving/Algorithms/Warmup/Birthday_Cake_Candles.py
@@ -1,24 +1,24 @@
 #! /usr/bin/python3
 
-'''
-You are in-charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. 
-When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow out.
 
-For example, if your niece is turning 4 years old, and the cake will have 4 candles of height 3, 2, 1, 3, she will be able to blow out 2 candles successfully, 
-since the tallest candle is of height 3 and there are 2 such candles.
+#You are in-charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. 
+#When she blows out the candles, she’ll only be able to blow out the tallest ones. 
+#Your task is to find out how many candles she can successfully blow out.
 
-Complete the function birthdayCakeCandles that takes your niece's age and an integer array containing height of each candle as input, 
-and return the number of candles she can successfully blow out.
+#For example, if your niece is turning 4 years old, and the cake will have 4 candles of height 3, 2, 1, 3, 
+#she will be able to blow out 2 candles successfully, since the tallest candle is of height 3 and there are 2 such candles.
 
-Input Format
+#Complete the function birthdayCakeCandles that takes your niece's age and an integer array containing height of each candle as input, 
+#and return the number of candles she can successfully blow out.
 
-The first line contains a single integer, n, denoting the number of candles on the cake.
-The second line contains n space-separated integers, where each integer i describes the height of candle i.
+#Input Format
 
-Output Format
+#The first line contains a single integer, n, denoting the number of candles on the cake.
+#The second line contains n space-separated integers, where each integer i describes the height of candle i.
 
-Print the number of candles that can be blown out on a new line.
-'''
+#Output Format
+
+#Print the number of candles that can be blown out on a new line.
 
 import math
 import os
