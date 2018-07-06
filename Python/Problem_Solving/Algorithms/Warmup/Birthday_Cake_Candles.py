114cd52742b0b2772e06a289dddea3df27df545c
pawelk82 <pawelk82@gmail.com>
2018-07-06 19:42:51 +0300

    add

--- Python/Problem_Solving/Algorithms/Warmup/Birthday_Cake_Candles.py
+++ Python/Problem_Solving/Algorithms/Warmup/Birthday_Cake_Candles.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python3
 
 
-#You are in-charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. 
-#When she blows out the candles, she’ll only be able to blow out the tallest ones. 
+#You are in-charge of the cake for your niece's birthday and have decided the cake will have one candle 
+#for each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones. 
 #Your task is to find out how many candles she can successfully blow out.
 
 #For example, if your niece is turning 4 years old, and the cake will have 4 candles of height 3, 2, 1, 3, 
 #she will be able to blow out 2 candles successfully, since the tallest candle is of height 3 and there are 2 such candles.
 
-#Complete the function birthdayCakeCandles that takes your niece's age and an integer array containing height of each candle as input, 
-#and return the number of candles she can successfully blow out.
+#Complete the function birthdayCakeCandles that takes your niece's age and an integer array containing height of each candle 
+#as input, and return the number of candles she can successfully blow out.
 
 #Input Format
 
